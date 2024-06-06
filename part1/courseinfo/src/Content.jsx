d80a95a49c9f8bb2b204c507a2435de2c912ca8e
alex <nxvembr@posteo.me>
2024-06-06 19:00:09 +0300

    courseinfo 1.5

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,11 +1,20 @@
 import Part from "./Part";
 
-const Content = ({ parts }) => {
+const Content = ({ course }) => {
   return (
     <>
-      <Part partName={parts[0].name} exercises={parts[0].exercises} />
-      <Part partName={parts[1].name} exercises={parts[1].exercises} />
-      <Part partName={parts[2].name} exercises={parts[2].exercises} />
+      <Part
+        partName={course.parts[0].name}
+        exercises={course.parts[0].exercises}
+      />
+      <Part
+        partName={course.parts[1].name}
+        exercises={course.parts[1].exercises}
+      />
+      <Part
+        partName={course.parts[2].name}
+        exercises={course.parts[2].exercises}
+      />
     </>
   );
 };
