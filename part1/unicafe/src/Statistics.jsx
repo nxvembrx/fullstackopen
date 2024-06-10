75e8e6f0a32f9dbc2e207601a7654190fcd3d83b
alex <nxvembr@posteo.me>
2024-06-10 18:16:14 +0300

    unicafe 1.9

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,14 +1,18 @@
 const Statistics = ({ total, ratings }) => {
-  return (
-    <>
-      <p>good {ratings[0]}</p>
-      <p>neutral {ratings[1]}</p>
-      <p>bad {ratings[2]}</p>
-      <p>all {total}</p>
-      <p>average {(ratings[0] - ratings[2]) / total || 0}</p>
-      <p>positive {(ratings[0] * 100) / total || 0}%</p>
-    </>
-  );
+  if (total === 0) {
+    return <p>No feedback given</p>;
+  } else {
+    return (
+      <>
+        <p>good {ratings[0]}</p>
+        <p>neutral {ratings[1]}</p>
+        <p>bad {ratings[2]}</p>
+        <p>all {total}</p>
+        <p>average {(ratings[0] - ratings[2]) / total || 0}</p>
+        <p>positive {(ratings[0] * 100) / total || 0}%</p>
+      </>
+    );
+  }
 };
 
 export default Statistics;
