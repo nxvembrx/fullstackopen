703a0742d81188f02b80632da83c867121226537
alex <nxvembr@posteo.me>
2024-06-11 18:44:20 +0300

    2.2 courseinfo

--- part2/courseinfo/src/components/CourseContent.jsx
+++ part2/courseinfo/src/components/CourseContent.jsx
@@ -1,12 +1,18 @@
 import CourseContentPart from "./CourseContentPart";
+import CourseTotal from "./CourseTotal";
 
 const CourseContent = ({ parts }) => {
   return (
-    <ul>
-      {parts.map((part) => (
-        <CourseContentPart part={part} key={part.id} />
-      ))}
-    </ul>
+    <>
+      <ul>
+        {parts.map((part) => (
+          <CourseContentPart part={part} key={part.id} />
+        ))}
+      </ul>
+      <CourseTotal
+        total={parts.reduce((sum, part) => sum + part.exercises, 0)}
+      />
+    </>
   );
 };
 
---
+++ part2/courseinfo/src/components/CourseTotal.jsx
@@ -0,0 +1,9 @@
+const CourseTotal = ({ total }) => {
+  return (
+    <p>
+      <strong>total of {total} exercises</strong>
+    </p>
+  );
+};
+
+export default CourseTotal;
