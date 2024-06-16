5177b664ff8e4189e98fd4ce96cb6c6036b327f7
alex <nxvembr@posteo.me>
2024-06-16 11:39:02 +0300

    3.11

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -8,6 +8,7 @@ morgan.token("body", (req, res) => JSON.stringify(req.body));
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("dist"));
 app.use(
   morgan(
     ":method :url :status :res[content-length] - :response-time ms :body",
