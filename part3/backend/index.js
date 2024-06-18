420a12894a92c8a80fa30e4c70c6b8ce8cb5758a
alex <nxvembr@posteo.me>
2024-06-18 14:55:36 +0300

    3.15

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -87,10 +87,9 @@ app.get("/api/persons/:id", (request, response) => {
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => response.status(204).end())
+    .catch((error) => next(error));
 });
 
 app.get("/info", (request, response) => {
