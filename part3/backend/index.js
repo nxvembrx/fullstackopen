4f0a4620bc30531c511e2d07c21156a603cca21e
alex <nxvembr@posteo.me>
2024-06-18 18:02:25 +0300

    3.16

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -99,6 +99,22 @@ app.get("/info", (request, response) => {
         `);
 });
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
