69cefd9fcbe1d84877dbc4cf2e78452f001c3b51
alex <nxvembr@posteo.me>
2024-06-19 19:23:32 +0300

    4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,7 +6,18 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => (sum += blog.likes), 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const maxLikes = Math.max(...blogs.map((blog) => blog.likes));
+  const favoriteBlog = blogs.find((blog) => blog.likes === maxLikes);
+  return {
+    title: favoriteBlog.title,
+    author: favoriteBlog.author,
+    likes: favoriteBlog.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
