beb9f05dc4638999928d8fd915c51ff97bc8d313
alex <nxvembr@posteo.me>
2024-06-23 09:18:06 +0300

    4.10 (2)

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -6,12 +6,11 @@ blogsRouter.get("/", async (_, response) => {
   return response.json(blogs);
 });
 
-blogsRouter.post("/", (request, response) => {
+blogsRouter.post("/", async (request, response) => {
   const blog = new Blog(request.body);
+  const result = await blog.save();
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  response.status(201).json(result);
 });
 
 module.exports = blogsRouter;
