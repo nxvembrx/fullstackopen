2943cee5744439d077485ceff23aaee03272abe9
alex <nxvembr@posteo.me>
2024-06-23 11:11:39 +0300

    4.15

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -7,6 +7,7 @@ const config = require("./utils/config");
 const { unknownEndpoint, errorHandler } = require("./utils/errorHandler");
 
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 
 mongoose.connect(config.MONGODB_URI);
 
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 app.use(unknownEndpoint);
 app.use(errorHandler);
