d6f6c814436a38088917fdc6924fdf7af43b3c41
alex <nxvembr@posteo.me>
2024-06-27 15:01:04 +0300

    5.10

--- part5/src/services/blogs.js
+++ part5/src/services/blogs.js
@@ -9,7 +9,7 @@ const setToken = (newToken) => {
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
-  return response.data;
+  return response.data.sort((a, b) => b.likes - a.likes);
 };
 
 const create = async (newBlog) => {
