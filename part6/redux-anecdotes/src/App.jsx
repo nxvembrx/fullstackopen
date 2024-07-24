26cff3d6b3c43bef494fa12a7aba9777c7c42315
alex <nxvembr@posteo.me>
2024-07-24 13:33:31 +0300

    6.4

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -8,6 +8,19 @@ const App = () => {
     dispatch({ type: "VOTE", payload: { id: id } });
   };
 
+  const addAnecdote = (event) => {
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+    dispatch({
+      type: "NEW_ANECDOTE",
+      payload: {
+        content,
+        votes: 0,
+      },
+    });
+  };
+
   return (
     <div>
       <h2>Anecdotes</h2>
@@ -21,11 +34,11 @@ const App = () => {
         </div>
       ))}
       <h2>create new</h2>
-      <form>
+      <form onSubmit={addAnecdote}>
         <div>
-          <input />
+          <input name="anecdote" />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
       </form>
     </div>
   );
