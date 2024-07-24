d5d43be6ab201e4e4abd04193357b66071cf21ab
alex <nxvembr@posteo.me>
2024-07-24 14:15:09 +0300

    6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -3,7 +3,9 @@ import { useSelector, useDispatch } from "react-redux";
 import { voteAnecdote } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector(({ filter, anecdotes }) => {
+    return anecdotes.filter((a) => a.content.match(new RegExp(filter, "gi")));
+  });
   const dispatch = useDispatch();
 
   const vote = (id) => {
